ed5eef4039f749c8eb4061af8a5eb226ec1eaca9
Krzysztof Szymanski <krzysztof@apple.com>
2022-12-15 16:41:17 +0100

    accepts res port format

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ print(all_ports)
 arduino_port = None
 
 for port in all_ports:
-    if port.name.find("usbserial"):
+    if port.name.find("usbserial") or port.name.find("ttyUSB"):
         arduino_port = port
         print(arduino_port.device)
 arduino = serial.Serial(port=arduino_port.device, baudrate=9600, timeout=.1)
