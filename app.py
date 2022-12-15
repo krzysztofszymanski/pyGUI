e5a9cf58ca0cfe49772f5f37ffe17a16420bc3b6
Krzysztof Szymanski <krzysztof@apple.com>
2022-12-15 12:28:15 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+
+from datetime import datetime
+import PySimpleGUI as sg
+
+
+
+if __name__ == '__main__':
+    vals =["1", "231", "321", "22221", "223231", "2323231", "3232321"]
+    box = sg.Listbox(
+        values= vals , enable_events=True, size=(100, 20),auto_size_text=True,
+        key="-SCROLL-WINDOW-"
+    )
+
+
+
+    for i in range(10):
+        vals.append("xxx")
+    box.scroll_width =100
+    box.scroll_arrow_width=100
+    layout = [[box], [sg.Button("OK"), sg.Button("TOP"), sg.Button("HOLD"), sg.Button("CLOSE")]]
+    window = sg.Window(title="Obrotnica", layout=layout, margins=(20, 20), no_titlebar=True, location=(0, 0), size=(1024, 600), keep_on_top=True)
+    while True:
+        event, values = window.read()
+        # End program if user closes window or
+        # presses the OK button
+        if event == "CLOSE":
+            break
+        if event == "TOP":
+            window["-SCROLL-WINDOW-"].update(vals, set_to_index=[0], scroll_to_index=0)
+        if event == "HOLD":
+            if (len(window["-SCROLL-WINDOW-"].get_indexes()) and window["-SCROLL-WINDOW-"].get_indexes()[0] == 0)\
+                    or len(window["-SCROLL-WINDOW-"].get_indexes()) == 0:
+                window["-SCROLL-WINDOW-"].update(vals, set_to_index=[1], scroll_to_index=1)
+        if event == "OK":
+            vals.insert(0, str(datetime.now()))
+            vals = vals[:1000]
+
+            index = 0
+            if len(window["-SCROLL-WINDOW-"].get_indexes()) and window["-SCROLL-WINDOW-"].get_indexes()[0]:
+                index = window["-SCROLL-WINDOW-"].get_indexes()[0] + 1
+            window["-SCROLL-WINDOW-"].update(vals, set_to_index=[index], scroll_to_index=index)
+            pass
+        elif event == sg.WIN_CLOSED:
+            break
+
+
+
